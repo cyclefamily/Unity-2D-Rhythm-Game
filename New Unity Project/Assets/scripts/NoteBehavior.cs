db4fe5a315752f86f774402a211940c886ce976a
cyclefamily <88062958+cyclefamily@users.noreply.github.com>
2021-07-28 13:39:56 +0900

    판정 가이드 및 판정선 생성

---
+++ New Unity Project/Assets/scripts/NoteBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour
+{
+    public int noteType;
+
+
+    void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
+        // 사용자가 노트 키를 입력한 경우
+     
+    }
+
+    
+
+}
